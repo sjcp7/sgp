0a5346f341a093feefaa0b6584be1f7f4b81fa21
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-13 21:37:00 +0100

    add more views

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,25 @@
 class CoursesController < ApplicationController
-  before_action :set_instance_vars, only: %i[ new create ]
+  before_action :set_instance_vars, only: %i[ new create edit update ]
+  before_action :set_course, only: %i[ show edit update ]
+  before_action :set_subjects, only: %i[ edit update ]
+
+  def index
+    @courses = Course.all
+  end
+
   def new
     @course = Course.new
+    @subjects = Subject.all
     @course.course_subjects.build
   end
 
-  def create 
+  def show
+  end
+
+  def edit    
+  end
+
+  def create
     @course = Course.create(course_params)
     if @course.save
       redirect_to root_path, notice: 'Curso criado com sucesso.'
@@ -15,15 +29,39 @@ class CoursesController < ApplicationController
     end
   end
 
+  def update
+    params[:course][:course_subjects_attributes].each do |attr_set|
+      if attr_set.last[:subject_id].blank?
+        attr_set.last[:_destroy] = '1'
+      end
+    end
+    if @course.update(course_params)
+      redirect_to @course, notice: 'Curso actualizado com sucesso.'
+    else
+      flash.now[:alert] = 'Não foi possível actualizar curso.'
+      render :edit
+    end
+  end
+
   private
 
   def course_params
-    params.require(:course).permit(:description, :acronymn, :kind, course_subjects_attributes: [:id, :course_id, :subject_id, :school_grade_id])
+    params.require(:course).permit(:description, :acronymn, :kind, course_subjects_attributes: [:id, :subject_id, :school_grade_id, :_destroy])
   end
 
   def set_instance_vars
-    @subjects = Subject.all
     @school_grades = SchoolGrade.all
     @course_kinds = Course.kinds.values
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def set_subjects
+    @subjects = {
+      in_course: Subject.in_course(@course),
+      not_in_course: Subject.not_in_course(@course)
+    }
   end
 end
