1c7ac9845cc3a789823072eed9ed1c86bff8dc9d
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-13 21:35:52 +0100

    add some querying methods

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,30 @@
 class Subject < ApplicationRecord
   has_many :course_subjects
   has_many :courses, through: :course_subjects
+
+  def self.in_course(course)
+    in_course = {}
+    set_hash(in_course)
+    course.course_subjects.each do |cs|
+      in_course[cs.school_grade.grade] << cs.subject
+    end
+    in_course
+  end
+
+  def self.not_in_course(course)
+    not_in_course = {}
+    set_hash(not_in_course)
+    in_course(course).each do |grade, subjects_array|
+      not_in_course[grade] = Subject.all - subjects_array
+    end
+    not_in_course
+  end
+
+  private
+
+  def self.set_hash(hash)
+    SchoolGrade.all.each do |grade|
+      hash[grade.grade] = []
+    end
+  end
 end
