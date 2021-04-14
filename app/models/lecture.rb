48ecb39a5f3afe404a378796f841dbcc73b7ae0a
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-14 15:18:38 +0100

    add students association

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -3,6 +3,7 @@ class Lecture < ApplicationRecord
   belongs_to :batch
   belongs_to :teacher
   has_one :school_year, through: :batch
+  has_many :students, through: :batch
   has_one :school_grade, through: :course_subject
   has_many :tests
 end
