2aca273d0258ed8f6f36b0c037589be6944a9516
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-14 17:02:59 +0100

    add authorization exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user!
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Você não está autorizado para fazer isso."
+    redirect_to(request.referrer || root_path)
+  end
 end
