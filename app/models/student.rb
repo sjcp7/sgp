f2904f9812c495514672662745f4599f2c6c4e37
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-15 00:27:39 +0100

    add full name method

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
   has_many :enrollments
   has_many :batches, through: :enrollments
   has_many :tests
+
+  def full_name
+    "#{first_name} #{last_name}" 
+  end
 end
