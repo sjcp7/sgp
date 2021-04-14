a91526bfc5532f17f50b054c2badc588d0a04400
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-14 15:33:03 +0100

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-  get 'batches/new'
-  get 'courses/new'
   devise_for :users
-  resources :teachers
-  resources :students
-  resources :subjects
-  resources :courses
-  resources :batches
+  resources :batches, only: %i[ index show ]
+  resources :lectures, only: %i[ show ] do
+    resources :tests, only: %i[ index ], shallow: true
+  end
+
+  scope '/admin' do
+    resources :teachers, :students, :courses, :subjects
+  end
+
+  namespace :admin do 
+    resources :batches do
+      resources :lectures, shallow: true do
+        resources :tests, shallow: true
+      end
+    end
+  end
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
