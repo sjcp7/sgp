be6ffd957368ebb76a4a2967dc4a10f02216c336
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-15 11:00:55 +0100

    add lectures with tests instance var

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,7 +1,12 @@
 class LecturesController < ApplicationController
   def index
-    @lectures = policy_scope(Lecture).where(batch_id: params[:batch_id])
     @batch = Batch.find(params[:batch_id])
+    @lectures = policy_scope(Lecture).where(batch: @batch)
     @school_quarters = SchoolQuarter.all
+    @lectures_with_tests = @lectures.map do |lecture|
+      @school_quarters.map do |sq|
+        { lecture: lecture, sq: sq, tests: lecture.tests.find_by_school_quarter(sq) }
+      end
+    end.first
   end
 end
