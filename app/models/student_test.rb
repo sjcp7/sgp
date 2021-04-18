fcce067a6622cc8a5bf8b0d188e507bbe57af3e8
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-18 20:25:05 +0100

    add a find_by_student scope

--- app/models/student_test.rb
+++ app/models/student_test.rb
@@ -7,9 +7,11 @@ class StudentTest < ApplicationRecord
 
   after_update :update_student_tests
 
+  scope :find_by_student, ->(student) { where(student: student) }
+
   def update_student_tests
     sq = self.test.school_quarter
-    tests = self.student.tests.where(lecture: self.test.lecture)
+    tests = self.student.tests.find_by_lecture(self.test.lecture)
     if self.test.AC?
       update_MAC(sq, tests)
     elsif self.test.PP? || self.test.PT? || self.test.MAC?
@@ -24,9 +26,9 @@ class StudentTest < ApplicationRecord
   private
 
   def update_MAC(sq, tests)
-    mac = tests.MAC.find_by_school_quarter(sq).first.student_tests.where(student: self.student).first
+    mac = tests.MAC.find_by_school_quarter(sq).first.student_tests.find_by_student(self.student).first
     acs = tests.AC.find_by_school_quarter(sq).map do |ac|
-      ac_test = ac.student_tests.where(student: self.student).first
+      ac_test = ac.student_tests.find_by_student(self.student).first
       if ac_test.test.max_score == 5
         ac_test.score * 4
       elsif ac_test.test.max_score == 10
@@ -38,10 +40,10 @@ class StudentTest < ApplicationRecord
   end
 
   def update_MT(sq, tests)
-    mac = tests.MAC.find_by_school_quarter(sq).first.student_tests.where(student: self.student).first
-    pp = tests.PP.find_by_school_quarter(sq).first.student_tests.where(student: self.student).first
-    pt = tests.PT.find_by_school_quarter(sq).first.student_tests.where(student: self.student).first
-    mt = tests.MT.find_by_school_quarter(sq).first.student_tests.where(student: self.student).first
+    mac = tests.MAC.find_by_school_quarter(sq).first.student_tests.find_by_student(self.student).first
+    pp = tests.PP.find_by_school_quarter(sq).first.student_tests.find_by_student(self.student).first
+    pt = tests.PT.find_by_school_quarter(sq).first.student_tests.find_by_student(self.student).first
+    mt = tests.MT.find_by_school_quarter(sq).first.student_tests.find_by_student(self.student).first
     if mac.test.course.puniv?
       update_MT_PUNIV(mac, pp, pt, mt)
     else
@@ -61,13 +63,13 @@ class StudentTest < ApplicationRecord
 
   def update_MT_Technical(p)
     past_sq = p[:sqs][p[:sqs].find_index(p[:sq]) - 1]
-    past_mt = p[:tests].MT.find_by_school_quarter(past_sq).first.student_tests.where(student: self.student).first
+    past_mt = p[:tests].MT.find_by_school_quarter(past_sq).first.student_tests.find_by_student(self.student).first
     average = (past_mt.score + p[:mac].score + p[:pp].score + p[:pt].score) / 4
     p[:mt].update(score: average)
   end
 
   def update_MAD(sq, tests)
-    mad = tests.MAD.first.student_tests.where(student: self.student).first
+    mad = tests.MAD.first.student_tests.find_by_student(self.student).first
     if self.test.course.technical?
       update_MAD_Technical(sq, tests, mad)
     elsif self.test.course.puniv?
@@ -78,7 +80,7 @@ class StudentTest < ApplicationRecord
   def update_MAD_Technical(sq, tests, mad)
     if sq.quarter == 3
       
-      mt = tests.MT.find_by_school_quarter(sq).first.student_tests.where(student: self.student).first
+      mt = tests.MT.find_by_school_quarter(sq).first.student_tests.find_by_student(self.student).first
       mad.update(score: mt.score)
     end
   end
@@ -86,7 +88,7 @@ class StudentTest < ApplicationRecord
   def update_MAD_PUNIV(sq, tests, mad)
     if sq.quarter == 3
       mts = tests.MT.map do |mt|
-        mt.student_tests.where(student: self.student).first.score
+        mt.student_tests.find_by_student(self.student).first.score
       end
       average = mts.reduce(:+) / 3
       mad.update(score: average)
@@ -94,9 +96,9 @@ class StudentTest < ApplicationRecord
   end
 
   def update_CF(tests)
-    cf = tests.CF.first.student_tests.where(student: self.student).first
-    mad = tests.MAD.first.student_tests.where(student: self.student).first
-    ex = tests.EX.first.student_tests.where(student: self.student).first
+    cf = tests.CF.first.student_tests.find_by_student(self.student).first
+    mad = tests.MAD.first.student_tests.find_by_student(self.student).first
+    ex = tests.EX.first.student_tests.find_by_student(self.student).first
     score = (mad.score * 0.4) + (ex.score * 0.6)
     cf.update(score: score)
   end
