dfbf4c43418823c0e2f70bb35389e94bf33aa970
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-19 17:22:20 +0100

    simplify query logic

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -3,10 +3,5 @@ class LecturesController < ApplicationController
     @batch = Batch.find(params[:batch_id])
     @lectures = policy_scope(Lecture).where(batch: @batch)
     @school_quarters = SchoolQuarter.all
-    @lectures_with_tests = @lectures.map do |lecture|
-      @school_quarters.map do |sq|
-        { lecture: lecture, sq: sq, tests: lecture.tests.find_by_school_quarter(sq) }
-      end
-    end.first
   end
 end
