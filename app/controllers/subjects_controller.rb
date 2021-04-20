610f9af1f82b9c0bdd7ffef1635a812e7c4123d0
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-20 18:42:41 +0100

    add index, edit, update actions

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,15 +1,33 @@
 class SubjectsController < ApplicationController
+  def index
+    @subjects = Subject.all
+  end
+
   def new
     @subject = Subject.new
+  end
+
+  def edit
+    @subject = Subject.find(params[:id])
   end
 
   def create
     @subject = Subject.create(subject_params)
     if @subject.save
-      redirect_to root_path, notice: 'Disciplina criada com sucesso.'
+      redirect_to subjects_path, notice: 'Disciplina criada com sucesso.'
     else
       flash.now[:alert] = 'Não foi possível criar disciplina.'
       render :new
+    end
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update(subject_params)
+      redirect_to subjects_path, notice: 'Disciplina actualizada com sucesso.'
+    else
+      flash.now[:alert] = 'Não foi possível actualizar disciplina.'
+      render :edit
     end
   end
 
