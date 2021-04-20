3a7a8671d032cab8fd68ad6a359b9765cc0333e6
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-20 18:24:34 +0100

    add full_name method

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,4 +5,8 @@ class Teacher < ApplicationRecord
   has_many :course_subjects, through: :lectures
   has_many :requests
   accepts_nested_attributes_for :user
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
