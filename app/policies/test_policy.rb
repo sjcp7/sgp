d140899ed5b3fb0d17194b78afa91f342452616a
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-27 22:02:00 +0100

    add test kind and refactor code into methods

--- app/policies/test_policy.rb
+++ app/policies/test_policy.rb
@@ -6,7 +6,7 @@ class TestPolicy < ApplicationPolicy
   end
 
   def index?
-    user.admin? or record.teacher == user.profile
+    user.admin? or user_owns_test?
   end
 
   def create?
@@ -14,6 +14,16 @@ class TestPolicy < ApplicationPolicy
   end
 
   def update?
-    user.admin? or (record.teacher == user.profile && (record.AC? or record.PP? or record.PT?))
+    user.admin? or (!record.locked? && (user_owns_test? && allowed_tests?))
+  end
+
+  private
+
+  def user_owns_test?
+    record.teacher == user.profile
+  end
+
+  def allowed_tests?
+    record.AC? || record.PP? || record.PT? || record.EX?
   end
 end
