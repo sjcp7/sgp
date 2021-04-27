687c08baecbb587177aa523a018767e331cd5056
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-27 22:43:13 +0100

    add managed batches association

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,7 @@ class Teacher < ApplicationRecord
   has_one :user, as: :profile, dependent: :destroy
   has_many :lectures
   has_many :batches, through: :lectures
+  has_many :managed_batches, class_name: 'Batch', foreign_key: :batch_director
   has_many :course_subjects, through: :lectures
   has_many :requests
   accepts_nested_attributes_for :user
