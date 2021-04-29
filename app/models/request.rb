9d25bf38b2080f8b4bd6d916abb742055d54aba1
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-29 10:37:09 +0100

    add associations and scope to filter by batch director

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,8 @@
 class Request < ApplicationRecord
   belongs_to :teacher
   belongs_to :test
+  has_one :batch, through: :test
+  has_one :batch_director, through: :test
+
+  scope :find_by_batches, ->(batches) { joins(:batch).where(batch: { id: [batches]}) }
 end
