ef375c5dbd09cb4eb89d2c2e52e99049b118b378
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-29 11:51:59 +0100

    add rules for batch directors

--- app/policies/request_policy.rb
+++ app/policies/request_policy.rb
@@ -1,19 +1,23 @@
 class RequestPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all if user.admin?
+      if user.admin?
+        scope.all
+      elsif user.teacher? && user.profile.managed_batches.any?
+        scope.find_by_batches(user.profile.managed_batches)
+      end        
     end
   end
 
   def index?
-    user.admin?
+    user.admin? or user.profile.managed_batches.any?
   end
 
   def create?
     true
   end
 
-  def show
-    user.admin?
+  def show?
+    user.admin? or record.batch_director == user.profile
   end
 end
