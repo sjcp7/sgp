92185ec6e602ab5e8b6e698a7a3c9ed59a74b3f4
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-04-29 10:36:43 +0100

    add policy scope and authorization

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,19 +1,23 @@
 class RequestsController < ApplicationController
   def index
-    @requests = Request.all
+    @requests = policy_scope(Request)
+    authorize @requests
   end
 
   def show
     @request = Request.find(params[:id])
+    authorize @request
   end
 
   def new
     @request = Request.new
     @test_id = params[:test_id]
+    authorize @request
   end
 
   def create
     @request = Request.new(request_params)
+    authorize @request
     if @request.save
       batch = @request.test.batch
       flash[:notice] = 'Pedido enviado com sucesso.'
